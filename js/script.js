305d21879c1e389f91f7338616ef3b6965358530
Romango <homer37@yandex.ru>
2021-08-28 17:03:20 +0400

    onemore

--- js/script.js
+++ js/script.js
@@ -2,23 +2,6 @@
 "use strict";
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмтрели?','');
-alert (numberOfFilms);
-const personalMoviesDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
 
-};
 
-const a = prompt('Один из последних просмотренных фильмов',''),
-      b = prompt('На сколько оцентие его?',''),
-      c = prompt('Один из последних просмотренных фильмов',''),
-      d = prompt('На сколько оцентие его?','');
-
-personalMoviesDB.movies[a] = b; 
-personalMoviesDB.movies[c] = d; 
-
-console.log(personalMoviesDB);
+if (4==9
