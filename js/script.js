e4d8ccd6478d433da2d7f0b24995024e17146eba
Romango <homer37@yandex.ru>
2021-08-28 23:08:54 +0400

    heelloooo

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
 
 "use strict";
-let fuck = {
-name:'bitch',
-familiname: 'whore',
-age: 22
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-if (4==4) {
-    console.log(fuck);
+
+for (let i = 1; i < 3; i++) {
+        let a = prompt('Один из последних просмотренных фильмов?', '');
+        let b = prompt('На сколько оцените его?', '');  
+if(a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+    personalMovieDB.movies[a] = b;   
+    console.log('done');        
+} else {
+    console.log('error');
+    i--;
 }
+
+     }
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+alert('Вы классический зритель');
+}
+else {
+    alert('Вы киноман');
+}
+
+console.log(personalMovieDB); 
