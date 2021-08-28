23e7906d2244617e55b72a20b2484eee0b5a9401
Romango <homer37@yandex.ru>
2021-08-28 09:31:34 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+"use strict";
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмтрели?','');
+alert (numberOfFilms);
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt('Один из последних просмотренных фильмов',''),
+      b = prompt('На сколько оцентие его?',''),
+      c = prompt('Один из последних просмотренных фильмов',''),
+      d = prompt('На сколько оцентие его?','');
+
+personalMoviesDB.movies[a] = b; 
+personalMoviesDB.movies[c] = d; 
+
+console.log(personalMoviesDB);
